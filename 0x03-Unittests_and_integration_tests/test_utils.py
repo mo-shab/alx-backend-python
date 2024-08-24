9d9d80b6510b85faf94fd592631e3847110d7839
Mohammed <mrcloud.med@gmail.com>
2024-08-24 14:58:39 +0200

    .\test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,8 +5,8 @@ from parameterized import parameterized
 
 import unittest
 
-access_nested_map = __import__('utils').access_nested_map
 
+access_nested_map = __import__('utils').access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -15,14 +15,22 @@ class TestAccessNestedMap(unittest.TestCase):
     """
     @parameterized.expand(
         [
-            ({"a" : 1}, {"a",}, 1),
-            ({"a": {"b": 2}}, {"a",}, {'b': 2}),
-            ({"a": {"b": 2}}, {"a", "b"}, 2),
+            ({"a" : 1}, ["a",], 1),
+            ({"a": {"b": 2}}, ["a",], {'b': 2}),
+            ({"a": {"b": 2}}, ["a", "b"], 2),
         ])
 
     def test_access_nested_map(self, nested_map, path, expected):
         """Methode test of access nested map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
-
-if __name__ == '__main__': 
-    unittest.main() 
+    
+    @parameterized.expand(
+            [
+                ({}, ["a",]),
+                ({"a": 1}, ["a", "b"]),
+            ]
+    )
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Methode test exceptions"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
