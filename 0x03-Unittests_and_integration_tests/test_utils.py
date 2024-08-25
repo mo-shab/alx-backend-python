51a618cba2fa7722170d5128eb82798f09278f78
Mohammed <mrcloud.med@gmail.com>
2024-08-25 18:56:59 +0200

    update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,7 @@
 
 from parameterized import parameterized
 from unittest.mock import patch, Mock
+from typing import Dict
 
 import unittest
 
@@ -21,7 +22,7 @@ class TestAccessNestedMap(unittest.TestCase):
             ({"a": {"b": 2}}, ["a", ], {'b': 2}),
             ({"a": {"b": 2}}, ["a", "b"], 2),
         ])
-    def test_access_nested_map(self, nested_map, path, expected):
+    def test_access_nested_map(self, nested_map, path, expected) -> None:
         """Methode test of access nested map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
@@ -31,7 +32,7 @@ class TestAccessNestedMap(unittest.TestCase):
                 ({"a": 1}, ["a", "b"]),
             ]
     )
-    def test_access_nested_map_exception(self, nested_map, path):
+    def test_access_nested_map_exception(self, nested_map, path) -> None:
         """Methode test exceptions"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
@@ -39,34 +40,15 @@ class TestAccessNestedMap(unittest.TestCase):
 
 class TestGetJson(unittest.TestCase):
     """Class for Test Get JSON methods"""
-    @patch('requests.get')
-    def test_get_json(self, mock_get):
-        """Methode to test get_json method"""
 
-        # Define the test Cases.
-        test_cases = [
-            {"test_url": "http://example.com", "test_payload":
-             {"payload": True}},
-            {"test_url": "http://holberton.io", "test_payload":
-             {"payload": False}},
-        ]
-        for case in test_cases:
-            test_url = case['test_url']
-            test_payload = case['test_payload']
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://http://holberton.io", {"payload": False}),
+    ])
 
-            # Set up the Mock response object with the desired json output
-            mock_response = Mock()
-            mock_response.json.return_value = test_payload
-            mock_get.return_value = mock_response
-
-            # Call the function with the test URL
-            result = get_json(test_url)
-
-            # Assert that the function returns the correct payload
-            self.assertEqual(result, test_payload)
-
-            # Assert that the correct URL was used in the GTE request
-            mock_get.assert_called_once_with(test_url)
-
-            # Reset the mock for the next iteration
-            mock_get.reset_mock()
+    def test_get_json(self, test_url: str, test_payload: Dict) -> None:
+        """Tests `get_json`'s output."""
+        attrs = {'json.return_value': test_payload}
+        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            req_get.assert_called_once_with(test_url)
